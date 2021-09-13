4396acd1b4633c0c2c21f31c3385e6ffbda049c4
edobobo <edoardo.barba94@gmail.com>
2021-09-13 16:43:59 +0200

    transformers and tokenizers logging turned off

---
+++ classy/scripts/model/__init__.py
@@ -0,0 +1,11 @@
+import transformers
+import os
+
+# setting the transformers logging level to error with their own function
+transformers.logging.set_verbosity_error()
+
+# todo: we should analyze this error a little bit. Gigi can be useful here.
+#  According to : https://stackoverflow.com/questions/62691279/how-to-disable-tokenizers-parallelism-true-false-warning
+#  We should check if we really need FastTokenizers instead of the plain tokenizer.
+# turning off the tokenizers parallelism warning
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
