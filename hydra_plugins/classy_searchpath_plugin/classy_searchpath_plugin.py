64e53a04cef176e6de5ae3c3daeb66091119d5eb
Valahaar <campolungo@di.uniroma1.it>
2021-10-03 12:23:37 +0200

    removed print from hydra plugin

--- hydra_plugins/classy_searchpath_plugin/classy_searchpath_plugin.py
+++ hydra_plugins/classy_searchpath_plugin/classy_searchpath_plugin.py
@@ -5,5 +5,4 @@ from hydra.plugins.search_path_plugin import SearchPathPlugin
 class ClassySearchPathPlugin(SearchPathPlugin):
     def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
         # from https://hydra.cc/docs/next/advanced/search_path/#creating-a-searchpathplugin
-        print("manipulated search path with classy's configs")
         search_path.append(provider="classy-searchpath-plugin", path="pkg://configurations")
