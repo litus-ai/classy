0c4d641f03f3497c754e34f54b219c387b2d8b88
Valahaar <campolungo@di.uniroma1.it>
2021-10-14 09:29:08 +0200

    docs for classy upload/download + installation

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -98,7 +98,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
                 // },
                 {
                   label: 'GitHub',
-                  href: 'https://github.com/facebook/docusaurus',
+                  href: 'https://github.com/sunglasses-ai/classy',
                 },
               ],
             },
