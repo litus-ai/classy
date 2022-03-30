44d8f86c7c62818bf99d00ea5b064bbd20e8795a
poccio <luigi.p.procopio@gmail.com>
2022-03-30 11:29:16 +0200

    [bugfixes] bugfixes on omegaconf resolvers

--- classy/__init__.py
+++ classy/__init__.py
@@ -1,1 +1,6 @@
+# omega conf resolvers
+from classy.data.dataset.hf.generation import (  # noqa
+    resolve_hf_generation_base_dataset_on_transformer_model,
+)
+from classy.utils.omegaconf import adapt_dataset_from  # noqa
 from classy.version import VERSION as __version__  # noqa
