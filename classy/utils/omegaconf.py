1ef67c384105ab08d26258b2d6a5bfd756e2aea8
poccio <luigi.p.procopio@gmail.com>
2022-04-20 16:50:04 +0200

    [bugfix] profiles bug fix

--- classy/utils/omegaconf.py
+++ classy/utils/omegaconf.py
@@ -6,12 +6,17 @@ from omegaconf import DictConfig, OmegaConf
 
 
 def adapt_dataset_from(training_dataset: DictConfig, setting: str):
+    # duplicate configuration
+    training_dataset = copy.deepcopy(training_dataset)
+    # resolve otherwise hydra.utils.instantiate will crash
+    OmegaConf.resolve(training_dataset)
+    # identify adaptation code
     train_dataset_class = training_dataset["_target_"]
     if not train_dataset_class.split(".")[-1][
         0
     ].isupper():  # if it is not upper then it is a class method
         train_dataset_class = ".".join(train_dataset_class.split(".")[:-1])
-    OmegaConf.resolve(training_dataset)
+    # invoke it and return
     return hydra.utils.instantiate(
         {"_target_": f"{train_dataset_class}.adapt_dataset_from"},
         training_dataset=training_dataset,
